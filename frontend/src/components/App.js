cd1b7949f90d4f147f03400f3e20be08488d5f9b
benzps01 <benzps01@gmail.com>
2024-07-31 08:17:55 +0530

    feat: add route paths

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import '../styles/App.css';
+import { Navbar } from './Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Doctors from './pages/Doctors';
+import Login from './pages/Login';
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className='App'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
