ae028190fb1f46e8568219842e869afa72baa1f5
benzps01 <benzps01@gmail.com>
2024-08-01 10:02:52 +0530

    update: show navbar only to only some routes where required.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import '../styles/App.css';
 import { Navbar } from './Navbar';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Doctors from './pages/Doctors';
 import Login from './pages/Login';
+import PatientLogin from './pages/PatientLogin';
+import DoctorLogin from './pages/DoctorLogin';
+import AdminLogin from './pages/AdminLogin';
 
 export default function App() {
+  const location = useLocation();
+  const NavbarRoutes = ['/', '/about', '/doctors', '/login'];
+  const showNavbar = NavbarRoutes.includes(location.pathname);
+
   return (
     <div className='App'>
-      <Navbar />
+      {showNavbar && <Navbar />}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/doctors' element={<Doctors />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/patientLogin' element={<PatientLogin />} />
+        <Route path='/doctorLogin' element={<DoctorLogin />} />
+        <Route path='/adminLogin' element={<AdminLogin />} />
       </Routes>
     </div>
   );
