617d713e50c79cd6aab506a3d5ecf3fe0df5a89b
benzps01 <benzps01@gmail.com>
2024-08-01 09:57:35 +0530

    handle common login component to route to respective login page

--- frontend/src/components/pages/Login.jsx
+++ frontend/src/components/pages/Login.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import '../../styles/LoginPage.css'
+import img1 from '../../images/patientImg1.png'
+import img2 from '../../images/doctorImg1.png'
+import img3 from '../../images/adminImg1.png'
+import { useNavigate } from 'react-router-dom'
 
 export default function Login() {
+
+  const navigate = useNavigate();
+
+  const handleClick = (route) => () => {
+    navigate(route);
+  }
+
   return (
-    <div>Login</div>
+    <div className="container">
+      <span onClick={handleClick('/patientLogin')}>
+        <img src={img1} alt="Patient"/>
+      </span>
+      <span onClick={handleClick('/doctorLogin')}>
+        <img src={img2} alt="Doctor"/>
+      </span>
+      <span onClick={handleClick('/adminLogin')}>
+        <img src={img3} alt="Admin"/>
+      </span>
+    </div>
   )
 }
