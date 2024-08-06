737f24c39a17c1f593c7d66e2323e311590f2d49
benzps01 <benzps01@gmail.com>
2024-08-06 19:19:31 +0530

    authentication response to send or receive tokens

---
+++ backend/src/main/java/com/healthsphere/health/entity/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package com.healthsphere.health.entity;
+
+public class AuthenticationResponse {
+
+	private String token;
+	
+	public AuthenticationResponse(String token) {
+		this.token = token;
+	}
+	
+	public String getToken() {
+		return token;
+	}
+	
+}
