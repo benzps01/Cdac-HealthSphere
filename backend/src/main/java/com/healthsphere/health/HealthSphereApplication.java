b26dc790a74576c3dee9cad59b3a9f9566277339
benzps01 <benzps01@gmail.com>
2024-08-06 19:08:50 +0530

    spring backend initializer

---
+++ backend/src/main/java/com/healthsphere/health/HealthSphereApplication.java
@@ -0,0 +1,13 @@
+package com.healthsphere.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthSphereApplication.class, args);
+	}
+
+}
