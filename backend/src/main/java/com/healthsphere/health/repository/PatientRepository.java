114b4aacf1c55111f4ef85890cdfaf96412c8a2d
benzps01 <benzps01@gmail.com>
2024-08-14 14:57:41 +0530

    remove abstract method

--- backend/src/main/java/com/healthsphere/health/repository/PatientRepository.java
+++ backend/src/main/java/com/healthsphere/health/repository/PatientRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.healthsphere.health.entity.Patients;
 
 public interface PatientRepository extends JpaRepository<Patients, Integer> {
-	Patients findByName(String name);
 	Patients findByUsername(String username);
 }
