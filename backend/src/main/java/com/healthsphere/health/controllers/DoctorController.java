3ea782395af65a33c90255afd3437e306eff4e1f
benzps01 <benzps01@gmail.com>
2024-08-14 14:40:30 +0530

    update doctor controller to add profile picture

--- backend/src/main/java/com/healthsphere/health/controllers/DoctorController.java
+++ backend/src/main/java/com/healthsphere/health/controllers/DoctorController.java
@@ -2,15 +2,20 @@ package com.healthsphere.health.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.healthsphere.health.entity.AuthenticationResponse;
 import com.healthsphere.health.entity.Doctors;
 import com.healthsphere.health.service.DoctorAuthenticationService;
@@ -31,12 +36,15 @@ public class DoctorController {
 	@Autowired
 	private JwtUtilService jwtUtilService;
 
-	@PostMapping("/register")
-	public ResponseEntity<AuthenticationResponse> registerDoctor(@RequestBody Doctors doctor) {
+	@PostMapping(value = "/register", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE })
+	public ResponseEntity<AuthenticationResponse> registerDoctor(@RequestPart("doc") String doc, @RequestPart("image") MultipartFile image) throws java.io.IOException {
 		try {
-			AuthenticationResponse response = doctorAuthService.register(doctor);
+			ObjectMapper objMapper = new ObjectMapper();
+			Doctors doctor = objMapper.readValue(doc, Doctors.class);
+			AuthenticationResponse response = doctorAuthService.register(doctor, image);
 			return ResponseEntity.status(HttpStatus.CREATED).body(response);
 		} catch (Exception e) {
+			e.printStackTrace();
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
 					.body(new AuthenticationResponse("Error registering Patient"));
 		}
@@ -48,6 +56,7 @@ public class DoctorController {
 			AuthenticationResponse response = doctorAuthService.authenticate(doctor);
 			return ResponseEntity.ok(response);
 		} catch (Exception e) {
+			e.printStackTrace();
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
 					.body(new AuthenticationResponse("Incorrect username or password"));
 		}
@@ -57,11 +66,22 @@ public class DoctorController {
 	public ResponseEntity<Doctors> getDoctor(@RequestHeader("Authorization") String token) {
 		String jwt = token.substring(7);
 		String username = jwtUtilService.extractUsername(jwt);
-		Doctors doctor = userService.getDoctorByName(username);
-
+		Doctors doctor = (Doctors) userService.loadUserByUsername(username);
 		if (doctor != null) {
 			return ResponseEntity.ok(doctor);
 		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
+	}
+	
+	@GetMapping("/picture/{doctorid}")
+	public ResponseEntity<byte[]> getProfilePic(@PathVariable int doctorid){
+		try {
+			byte[] image = doctorAuthService.getProfilePic(doctorid);
+			return ResponseEntity.ok()
+					.contentType(MediaType.IMAGE_JPEG)
+					.body(image);
+		}catch(RuntimeException e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 		}
 	}
