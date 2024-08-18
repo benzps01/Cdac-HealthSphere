07169a76d7b388e7cab5dec0c4a946093156818e
benzps01 <benzps01@gmail.com>
2024-08-19 00:29:26 +0530

    unnecessary cache files

--- AppointmentBackend/obj/Debug/net9.0/Test.AssemblyInfo.cs
+++ AppointmentBackend/obj/Debug/net9.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98cf1c8f8be1637271ebc93632209d92875621b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc4a03883f4fd8521319d3547f6caebdfe15a063")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
