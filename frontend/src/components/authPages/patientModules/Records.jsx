dc4a03883f4fd8521319d3547f6caebdfe15a063
benzps01 <benzps01@gmail.com>
2024-08-19 00:22:15 +0530

    create ehr data as table

--- frontend/src/components/authPages/patientModules/Records.jsx
+++ frontend/src/components/authPages/patientModules/Records.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import '../../../styles/Records.css'
+import test from '../../../images/about.jpg'
 
 export default function Records() {
   return (
-    <div>Records</div>
+    <div className='record-container'>
+      <table>
+        <thead>
+          <th>Sr.No</th>
+          <th>EHR ID</th>
+          <th>Doctor</th>
+          <th>Diagnosis</th>
+          <th>Prescriptions</th>
+          <th>Visit Date</th>
+          <th>Treatment</th>
+          <th>Notes</th>
+          <th>XRAY</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>Dr. John Doe</td>
+            <td>Has Chest Pain</td>
+            <td>Amlodipine 5mg</td>
+            <td>2023-12-31</td>
+            <td>Lifestyle changes</td>
+            <td>Patient advised to reduce salt intake</td>
+            <td>
+              <img src={test} alt='test'/>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
