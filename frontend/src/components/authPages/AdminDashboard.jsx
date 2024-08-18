0b569df6a838c8b59fa737a973c826d3a1645686
abhishekdesai25 <abhi2534desai@gmail.com>
2024-08-18 20:29:19 +0530

    Create doctor registration form.

--- frontend/src/components/authPages/AdminDashboard.jsx
+++ frontend/src/components/authPages/AdminDashboard.jsx
@@ -2,10 +2,15 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../AuthContext';
+import '../../styles/Dashboard.css'
+import RegisterDoctor from './adminModules/RegisterDoctor';
+import defaultImg from '../../images/default.jpg';
 
-export default function AdminDashboard() {
+export default function AdminDashboard() {  
 
     const [admin, setAdmin] = useState(null);
+    const [profileImage, setProfileImage] = useState(null);
+    const [activeComponent,setActiveComponent] = useState(false);
     const baseUrl = 'http://localhost:7070/health/admin';
     const navigate = useNavigate();
     const { setAuthState } = useAuth();
@@ -23,18 +28,46 @@ export default function AdminDashboard() {
                 headers: { Authorization: `Bearer ${token}`},
             });
             setAdmin(response.data);
+
+            if(response.data.profilepic){
+                const imageUrl = `data:image/png;base64,${response.data.profilepic}`;
+                setProfileImage(imageUrl);
+            }
         };
 
         fetchPatientDetails();
     },[]);
 
+    const handleShowRegisterDoctor = () => {
+        setActiveComponent(activeComponent === 'registerDoctor' ? null : 'registerDoctor');
+    }
+
+
     if(!admin) return <div>Loading.....</div>
 
   return (
-    <div>
-        <h1>Welcome, {admin.username}</h1>
-        <p>Phone Number: {admin.password}</p>
-        <button onClick={handleLogout} className='logout'>Logout</button>
+    <div className='admin-dashboard'>
+        <div className='profile'>
+            <img src = {profileImage || defaultImg} alt={admin.name} className='profileImg' />
+            <div className='content'>
+                <h2>Hi, {admin.name}</h2>
+            </div>
+            <hr/>
+            <br />
+            <button className='register' onClick={handleShowRegisterDoctor}>Doctor Registration</button>
+            <br/>
+            <button onClick={handleLogout} className='logout'>Logout</button>
+        </div>
+        {activeComponent === 'registerDoctor' && (
+            <div className='admin-container'>
+                <RegisterDoctor />
+            </div>
+        )}
     </div>
   );
 };
+
+
+
+
+
