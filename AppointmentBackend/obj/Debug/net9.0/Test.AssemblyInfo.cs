8c022a1a79b8060b288b0e50f4ce407b9e043df9
Benson P Sabu <benzps01@gmail.com>
2024-08-19 23:57:22 +0530

    merging from dev-ben to main

--- AppointmentBackend/obj/Debug/net9.0/Test.AssemblyInfo.cs
+++ AppointmentBackend/obj/Debug/net9.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc4a03883f4fd8521319d3547f6caebdfe15a063")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e486e1ebc2d0d054cf05f639bc8b39474cdf56f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
