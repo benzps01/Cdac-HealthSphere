067d5070412c6aa86691b4c27c470b339071e6d4
Shisheer043 <shishirghutugade@gmail.com>
2024-11-22 15:26:24 +0530

    all code

--- AppointmentBackend/obj/Debug/net9.0/Test.AssemblyInfo.cs
+++ AppointmentBackend/obj/Debug/net9.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ef11d41ad9cdd701c39b5586bf6b921e54ba838")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82c73782d3679f6c9329bf5f55e5060b0a788d71")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
