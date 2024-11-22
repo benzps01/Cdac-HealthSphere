03fd2abe73e8bc72ee490365dc89b12b0eef118b
Shisheer043 <shishirghutugade@gmail.com>
2024-11-22 16:16:25 +0530

    delete

--- backend/src/main/java/com/healthsphere/health/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.healthsphere.health.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.healthsphere.health.entity.Admins;
-
-public interface AdminRepository extends JpaRepository<Admins, Integer> {
-	Admins findByUsername(String username);
-}
